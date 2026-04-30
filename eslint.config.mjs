44e28129b149bdcacfd15027b4d71f3fd1f1e523
Tribus Loja Oficial <tribuslojaoficial@gmail.com>
2026-04-30 18:30:41 -0300

    chore: remove Next.js core web vitals rules from ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
-import nextVitals from "eslint-config-next/core-web-vitals";
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  ...nextVitals,
   {
     rules: {
       "@typescript-eslint/no-unused-vars": [
